c59eb551ca83db3a05c7f1351d6f5ebe70ab7aab
moaazEl <141212828+moaazEl@users.noreply.github.com>
2024-05-23 15:33:08 +1000

    Add files via upload

---
+++ register no db.py
@@ -0,0 +1,37 @@
+from flask import Flask
+from flask import Flask, flash, redirect, render_template, request, redirect,url_for
+
+
+app = Flask(__name__)
+app.secret_key = "donottellanyone"
+
+@app.route('/')
+def home():
+     return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return render_template("about.html" )
+
+
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    if request.method=='POST':
+        print("in post register")
+        user_name = request.form['user_name']
+        first_name = request.form['first_name']
+        last_name = request.form['last_name']
+        email = request.form['email']
+        if user_name == "Fred":
+            flash('Username already taken!')
+            return redirect(url_for("register"))
+        else:
+            flash('Registeration successful')
+            return render_template('register.html')
+    else:  #in get process
+        print("in get register")
+        return render_template('register.html')
+
+
+if __name__ == "__main__":
+    app.run()
